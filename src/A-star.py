f71d6ae4e6170f015cc0eacfaf941d105d935e62
YiwloveEric <125286621+YiwloveEric@users.noreply.github.com>
2024-08-06 15:12:59 +0800

    finish the path finding function but some problem occur , in and out are both one route

--- src/A-star.py
+++ src/A-star.py
@@ -12,27 +12,43 @@ import heapq
 import networkx as nx
 import numpy as np
 from scipy.spatial.distance import euclidean
+import matplotlib.pyplot as plt
 
 from CDT_Graph import SG_graph
 
 
 class PriorityQueue:
+    """
+    imitate the priorityqueue by heapq
+    """
     def __init__(self) -> None:
         self.elements: list[tuple[float, tuple[np.float64, np.float64]]] = []
 
     def empty(self) -> bool:
+        """
+        the priorityqueue empty or not
+        """
         return not self.elements
 
     def put(self, point: tuple[np.float64, np.float64], priority: float) -> None:
+        """
+        put the item into the priorityqueue
+        """
         heapq.heappush(self.elements, (priority, point))
 
     def get(self) -> tuple[np.float64, np.float64]:
+        """
+        get the top one
+        """
         return heapq.heappop(self.elements)[1]
 
 
 def heuristic(
     a: tuple[np.float64, np.float64], b: tuple[np.float64, np.float64]
 ) -> float:
+    """
+    Manhanton distance
+    """
     (x1, y1) = a
     (x2, y2) = b
     return abs(x1 - x2) + abs(y1 - y2)
@@ -43,6 +59,9 @@ def a_star_search(
     start: tuple[np.float64, np.float64],
     goal: tuple[np.float64, np.float64],
 ):
+    """
+    a searching algorithm to find the best path
+    """
     frontier = PriorityQueue()
     frontier.put(start, 0)
     came_from: dict[tuple[np.float64, np.float64], tuple[np.float64, np.float64]] = {}
@@ -66,31 +85,124 @@ def a_star_search(
 
     return came_from, cost_so_far
 
+def reconstruct_path(came_from: dict[tuple[np.float64, np.float64], tuple[np.float64, np.float64]],
+                     start: tuple[np.float64, np.float64], goal: tuple[np.float64, np.float64]) -> list[tuple[np.float64, np.float64]]:
+    """
+    reverse the path because when recoring the path is reverse
+    """
+    current: tuple[np.float64, np.float64] = goal
+    path: list[tuple[np.float64, np.float64]] = []
+    if goal not in came_from: # no path was found
+        return []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.append(start) # optional
+    path.reverse() # optional
+    return path
+
+def find_shortest_path(start_name:str,targe_name:str,compo_dict:dict):
+    shortest_path = None
+    shortest_distance = float('inf')
+    best_start = None
+    best_target = None
+
+    for start in compo_dict[start_name]:
+        for target in compo_dict[targe_name]:
+            start = tuple(np.float64(x) for x in start)
+            target = tuple(np.float64(x) for x in target)
+            # print(start,target)
+            came_from, cost_so_far = a_star_search(sg.graph, start, target)
+            if target in cost_so_far and cost_so_far[target] < shortest_distance:
+                shortest_distance = cost_so_far[target]
+                shortest_path = reconstruct_path(came_from, start, target)
+                best_start = start
+                best_target = target
+
+    return best_start,best_target,shortest_path
+
+def construct_path(data_path:str,nearest_incomp:dict,nearest_outcomp:dict,compo_dict:dict):
+    all_path:list[list[tuple[np.float64,np.float64]]] = []
+    with open(data_path, mode="r", encoding="utf-8") as f:
+        line = f.read().split("\n")
+        # print(line)
+    route_list = [lin.split('\t') for lin in line]
+    # print(route_list)
+
+    # process complete route path
+    for route in route_list:
+        route.insert(0,nearest_incomp[route[0]])
+        # ignore * first
+        if('*' in route[-1]):
+            route[-1] = route[-1].replace('*','')
+        route.append(nearest_outcomp[route[-1]])
+        # print(route)
+        # store each path
+        each_path_list : list[tuple[np.float64,np.float64]] = []
+        for idx in range(len(route)-1):
+            start_name = route[idx]
+            targe_name = route[idx+1]
+            best_start,best_target,shortest_path = find_shortest_path(start_name,targe_name,compo_dict)
+            each_path_list.append(shortest_path)
+        all_path.append(each_path_list)
+
+    return all_path
+
+
 
 if __name__ == "__main__":
-    # point1 = (np.float64(6), np.float64(1))
-    # point2 = (np.float64(6), np.float64(6))
-    # print(euclidean(point1,point2))
+    # # point1 = (np.float64(6), np.float64(1))
+    # # point2 = (np.float64(6), np.float64(6))
+    # # print(euclidean(point1,point2))
     sg = SG_graph("Data\data1.txt", "Data\input1.txt", 0.6)
     sg.add_midpoint_to_SG()
     sg.add_egdes_to_SG()
     sg.add_startarget_to_SG()
+    path_list = construct_path("Data\input1.txt",sg.nearest_incomp,sg.nearest_outcomp,sg.compo_dict)
+    # print(path_list)
+
+    colors = ['blue', 'black', 'red', 'purple', 'orange']
+
+    for i,each_path in enumerate(path_list):
+        plt.figure(figsize=(8, 6))
+        color = colors[i % len(colors)]
+        for partial_path in each_path:
+            x_coords, y_coords = zip(*partial_path)
+            plt.plot(x_coords, y_coords, marker='o', color=color, linestyle='-', linewidth=2, markersize=6)
+        sg.cdt.dispaly_cdt()
+
+    # # sg.draw_midpoint_and_neutrality()
+    # with open("Data\input1.txt", mode="r", encoding="utf-8") as f:
+    #     content = f.readline().strip()
+    #     # print(content)
+    #     first_name = content.split("\t")[0]
+    #     second_name = content.split("\t")[1]
+    #     print("the test components are:", first_name, second_name)
+    #     start_name = sg.nearest_incomp[first_name]
+    #     target_name = sg.nearest_outcomp[second_name]
+    #     # target = sg.compo_dict[first]
+    #     print("the input component and output component are:",start_name,target_name)
+
+    # shortest_path = None
+    # shortest_distance = float('inf')
+    # best_start = None
+    # best_target = None
+
+    # for start in sg.compo_dict[start_name]:
+    #     for target in sg.compo_dict[first_name]:
+    #         start = tuple(np.float64(x) for x in start)
+    #         target = tuple(np.float64(x) for x in target)
+    #         # print(start,target)
+    #         came_from, cost_so_far = a_star_search(sg.graph, start, target)
+    #         if target in cost_so_far and cost_so_far[target] < shortest_distance:
+    #             shortest_distance = cost_so_far[target]
+    #             shortest_path = reconstruct_path(came_from, start, target)
+    #             best_start = start
+    #             best_target = target
+
+    # # print(best_start,best_target)
+    # print(shortest_path)
+    # x_coords, y_coords = zip(*shortest_path)
+    # plt.plot(x_coords, y_coords, marker='o', color='black', linestyle='-', linewidth=2, markersize=6)
     # sg.draw_midpoint_and_neutrality()
-    with open("Data\input1.txt", mode="r", encoding="utf-8") as f:
-        content = f.readline().strip()
-        # print(content)
-        first = content.split("\t")[0]
-        second = content.split("\t")[1]
-        print("the test components are:", first, second)
-        start = sg.nearest_incomp[first]
-        target = sg.compo_dict[first]
-        # print(target)
-        # print(start)
-        
-        for start_ver in sg.compo_dict[start]:
-            start_ver = tuple(np.float64(x) for x in start_ver)
-
-
-        for target_ver in sg.compo_dict[first]:
-            # 转换成tuple[np.float64,np.float64]
-            target_ver = tuple(np.float64(x) for x in target_ver)
+    # # print(list(sg.graph.edges()))
